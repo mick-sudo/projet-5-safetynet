06935d482768884c956d79d4a57486cf6c1a0cf7
mickael j <joseph.mickk@gmail.com>
2021-06-25 11:27:09 +0200

    init

---
+++ src/main/java/com/safetynet/mickael/MickaelApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.mickael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MickaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MickaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/mickael/MickaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.mickael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MickaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
